a9c949e020dcab943747ed068076c8b737b7955e
Himanshu Tomar <himanshutomar3031@gmail.com>
2023-04-20 18:51:12 +0530

    added better approach

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -5,3 +5,13 @@ def two_number_sum(array, target_sum):
             if array[i] + array[j] == target_sum:
                 return [array[i], array[j]]
     return []
+
+#time O(N), space 0(N)
+def two_number_sum(array, target_sum):
+    complement_set = set()
+    for num in array:
+        complement = target_sum - num
+        if complement in complement_set:
+            return [complement, num]
+        complement_set.add(num)
+    return []
